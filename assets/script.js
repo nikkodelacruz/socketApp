5e143d706b608195ded4b4b5a28aca608e022bcf
Nikko <nikkodc12@gmail.com>
2020-04-25 00:14:18 +0800

    added styles, working chat

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,34 @@
 let app = new Vue({
-	el: '#app',
+	el: '#chatbox',
 	data: {
 		socket: null,
-		message: ''
+		message: '',
+		messages: [],
+		created: false,
+		user: '',
 	},
 	methods: {
 		sendMessage: function(){
-			this.socket.emit('message', this.message);
+			this.socket.emit('message', this.message); // send message
 			this.message = '';
-
+			// $(".messenger-container__conversation").animate({scrollTop: $(".messenger-container__conversation").get(0).scrollHeight},1);
+		},
+		createUser: function(){
+			this.socket.emit('join', this.user);
+			this.created  = true;
 		}
-
 	},
 	created: function() {
-		this.socket = io();
-		console.log(this.socket)
+		// this.socket = io();
+		this.socket = io.connect("http://localhost:3000/");
+		// var socket = io.connect("https://wedding-planning-chat-app.herokuapp.com/");
+		// console.log(this.socket)
+	},
+	mounted: function() {
+		let vm = this;
+		this.socket.on('message', function(message){
+			// alert()
+			vm.messages.push(message);
+		});
 	}
 });
--- chat.js
+++ chat.js
@@ -4,19 +4,27 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http)
 
 // include assets folder
-app.use(express.static(__dirname + '/assets'));
+// app.use(express.static(__dirname + '/assets'));
 
 // route
 app.get('/', (req, res) => {
 	// res.send('<h1>Test</h1>');
-	res.sendFile(__dirname + '/index.html');
+	// res.sendFile(__dirname + '/index.html');
 });
 
 // trigger on first run
 io.on('connection', (socket) => {
 	console.log('a user connected');
-	socket.on('message', (msg) => { // call emit "message"
-		console.log(msg);
+	socket.username = '';
+	socket.on('message', (msg) => { // listen from emit "message"
+		io.emit('message', { 'user': socket.username, 'message': msg }); // 
+	});
+
+	socket.on('join', (username) => {
+		if (username != null) {
+			socket.username = username;
+		}
+		socket.broadcast.emit('message', { joined: true, message: socket.username + ' Someone has joined' });
 	});
 });
 
